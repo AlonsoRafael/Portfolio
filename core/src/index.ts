ac04a21677cb2df40d310054d50716193cce01e3
Rafael Alonso <rafaalonsomarques@hotmail.com>
2026-03-19 16:07:43 -0300

    feat core : modelo implementado

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,3 +1,4 @@
-const x = 123
+export * from "./projeto"
+export * from "./tecnologia"
+export * from "./shared"
 
-export {x} 
---
+++ core/src/projeto/Tipo.ts
@@ -0,0 +1,10 @@
+export enum Tipo {
+    JOGO = "Jogo",
+    WEB = "Web",
+    MOBILE = "Mobile",
+    DESKTOP = "Desktop",
+    FULLSTACK = "Full Stack",
+    BACKEND = "Backend",
+    FRONTEND = "Frontend",
+    AUTOMACAO = "Automação",
+}
